67265b02cf05c661aa2a0de056977a97bca921b3
Prisma Data Platform <hello@prisma.io>
2021-09-15 15:11:51 +0000

    feat: setup Prisma schema project

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,42 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import * as faker from 'faker'
+
+const NUMBER_OF_USERS = 4
+const MAX_NUMBER_OF_LINKS = 5
+
+const data = Array.from({ length: NUMBER_OF_USERS }).map(() => ({
+  email: faker.internet.email(),
+  name: faker.name.firstName(),
+  links: Array.from({
+    length: faker.datatype.number({
+      min: 0,
+      max: MAX_NUMBER_OF_LINKS,
+    }),
+  }).map(() => ({
+    url: faker.internet.url(),
+    shortUrl: faker.internet.domainWord(),
+  })),
+}))
 
 export async function seed() {
-  const prisma = new PrismaClient();
+  const prisma = new PrismaClient()
+
+  try {
+    for (let entry of data) {
+      await prisma.user.create({
+        data: {
+          name: entry.name,
+          email: entry.email,
+          links: {
+            create: entry.links,
+          },
+        },
+      })
+    }
+  } catch (e) {
+    await prisma.$disconnect()
+    throw e
+  } finally {
+    await prisma.$disconnect()
+  }
 }
